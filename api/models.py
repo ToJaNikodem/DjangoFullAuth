0e893cc72e17663e23ffdf9a0f34bb9d5e88c181
ToJaNikodem <nikodembuczkowski05@gmail.com>
2024-02-23 02:34:10 +0100

    validation

--- api/models.py
+++ api/models.py
@@ -1,28 +1,39 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator, MinLengthValidator
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-    
+
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         if password:
             user.set_password(password)
+        try:
+            user.full_clean()
+        except ValidationError:
+            pass
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(username, email, password, **extra_fields)
-    
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     objects = CustomUserManager()
+
+    username = models.CharField(
+        max_length=40,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex='^[a-z0-9](?:[a-z0-9]+[.\-_]?)+[a-z0-9]$',
+                message='Wrong username!',
+                code='invalid_username'
+            ),
+            MinLengthValidator(4)
+        ]
+    )
+
+    def __str__(self):
+        return self.username
