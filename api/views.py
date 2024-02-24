5cf3b6fadf2a5e9048efe88a1521b3f23bc0fa15
ToJaNikodem <nikodembuczkowski05@gmail.com>
2024-02-25 00:27:11 +0100

    error handling

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
+from .models import CustomUser
+from .serializers import UserSerializer
 from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from .serializers import UserSerializer
-from .models import CustomUser
 from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
@@ -17,13 +18,18 @@ def user_delete(request):
     username = data.get('username')
     password = data.get('password')
 
-    user = CustomUser.objects.filter(username=username).first()
-    if user is not None and user.check_password(password):
-        # Delete the user
-        user.delete()
-        return Response({'message': 'User deleted successfully'}, status=status.HTTP_200_OK)
-    else:
-        return Response({'error': 'Invalid username or password'}, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        user = CustomUser.objects.get(username=username)
+        if user.check_password(password):
+            # Delete the user
+            user.delete()
+            return Response({'message': 'User deleted successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid username or password'}, status=status.HTTP_400_BAD_REQUEST)
+    except ObjectDoesNotExist:
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['POST'])
 def signup(request):
@@ -37,6 +43,6 @@ def signup(request):
             email=data['email'],
             password=data['password']
         )
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
