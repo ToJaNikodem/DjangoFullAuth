3f823d9321b9e9f148e3600b1a6482ee77a2a059
ToJaNikodem <nikodembuczkowski05@gmail.com>
2024-02-28 11:06:58 +0100

    test

--- FullAuthApp/settings.py
+++ FullAuthApp/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# test
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
