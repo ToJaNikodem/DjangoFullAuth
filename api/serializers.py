d4d84de7608db18b31cc7619809339e33bdfb883
ToJaNikodem <nikodembuczkowski05@gmail.com>
2024-03-17 22:20:07 +0100

    some error handling and validation

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,44 @@
 from rest_framework import serializers
 from .models import CustomUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.core.validators import RegexValidator, MinLengthValidator
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_username(self, value):
+        regex_validator = RegexValidator(
+            regex='^[a-z0-9](?:[a-z0-9]+[.\-_]?)+[a-z0-9]$',
+            message='Wrong username format!',
+            code='invalid_username'
+        )
+        min_length_validator = MinLengthValidator(4)
+        try:
+            regex_validator(value)
+            min_length_validator(value)
+        except Exception as e:
+            raise serializers.ValidationError(str(e))
+        return value
+
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError('already_taken')
+        return value
+    
+    def validate_password(self, value):
+        if len(value) < 10:
+            raise serializers.ValidationError('to_short')
+
+        if not any(char.isdigit() for char in value):
+            raise serializers.ValidationError('no_digit')
+
+        special_characters = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+        if not any(char in special_characters for char in value):
+            raise serializers.ValidationError('no_special')
+        return value
         
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
--- api/views.py
+++ api/views.py
@@ -15,76 +15,87 @@ from django.contrib.auth.tokens import default_token_generator
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def token_test(request):
-    return Response(status=status.HTTP_200_OK)
+    return Response({'message': 'Token is valid!'}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def user_delete(request):
     data = request.data
-    username = data.get('username')
-    password = data.get('password')
-
     try:
+        username = data.get('username', '')
+        password = data.get('password', '')
         user = CustomUser.objects.get(username=username)
+        
         if user.check_password(password):
             user.delete()
-            return Response({'message': 'User deleted successfully'}, status=status.HTTP_200_OK)
+            return Response({'message': 'User deleted successfully!'}, status=status.HTTP_200_OK)
         else:
-            return Response({'error': 'Invalid username or password'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'Invalid password!'}, status=status.HTTP_400_BAD_REQUEST)
     except ObjectDoesNotExist:
-        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'message': 'User does not exist!'}, status=status.HTTP_404_NOT_FOUND)
     except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'message': f'An error occurred: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['POST'])
 def email_verification(request):
     data = request.data
-    token = data.get('token')
-    user_id = data.get('user_id')
     try:
-        uid = urlsafe_base64_decode(user_id)
-        user = CustomUser.objects.get(pk=uid)
-    except Exception as e:
-        return Response({'message': 'error'}, status=status.HTTP_400_BAD_REQUEST)
-        
+        verification_token = data.get('token', '')
+        user_id = data.get('user_id', '')
 
-    if default_token_generator.check_token(user, token):
-        user.is_email_verified = True
-        user.save()
-        return Response({'detail': 'Email verified successfully'}, status=status.HTTP_200_OK)
-    else:
-        return Response({'detail': 'Invalid verification link'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            decoded_user_id = urlsafe_base64_decode(user_id)
+        except TypeError:
+            return Response({'message': 'Invalid verification link!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = CustomUser.objects.get(pk=decoded_user_id)
+
+        if user and default_token_generator.check_token(user, verification_token):
+            user.is_email_verified = True
+            user.save()
+            return Response({'message': 'Email verified successfully!'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid verification link!'}, status=status.HTTP_400_BAD_REQUEST)
+
+    except CustomUser.DoesNotExist: 
+        return Response({'message': 'Invalid verification link!'}, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({'message': f'An error occurred: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(['POST'])
 def signup(request):
     data = request.data
+    try:
+        serializer = UserSerializer(data=data)
 
-    serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            CustomUser.objects.create_user(
+                username=data.get('username', ''),
+                email=data.get('email', ''),
+                password=data.get('password', '')
+            )
+            user = CustomUser.objects.get(username=data.get('username', ''))
+            user.full_clean()
+            user.save()
+            
+            try:
+                subject = 'Activate Your Account'
+                message = render_to_string('verification_email.html', {
+                    'user': user,
+                    'domain': 'http://localhost:8080',
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': default_token_generator.make_token(user),
+                })
+                email = EmailMessage(subject, message, to=[user.email])
+                email.content_subtype = 'html'
+                email.send()
+            except Exception:
+                return Response({'message': 'User created successfully!', 'email': 'Email not send!'}, status=status.HTTP_201_CREATED)
 
-    if serializer.is_valid():
-        CustomUser.objects.create_user(
-            username=data['username'],
-            email=data['email'],
-            password=data['password']
-        )
-        user = CustomUser.objects.get(username=data['username'])
-
-        try:
-            subject = 'Activate Your Account'
-            message = render_to_string('verification_email.html', {
-                'user': user,
-                'domain': 'http://localhost:8080',
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': default_token_generator.make_token(user),
-            })
-            email = EmailMessage(subject, message, to=[user.email])
-            email.content_subtype = "html"
-            email.send()
-        except Exception as e:
-            print (e)
-            return Response({'message': 'Email not send'}, status=status.HTTP_200_OK)
-
-        return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'User created successfully!'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({'message': f'An error occurred: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
